d749707c354e7852d6bdb31b707da22410f55cc7
Jisu Sim <rlj1202@gmail.com>
2023-04-19 00:37:51 +0900

    Separate article metadata components

--- src/components/theme/Articles/ArticleCard.tsx
+++ src/components/theme/Articles/ArticleCard.tsx
@@ -4,7 +4,10 @@ import Link from 'next/link';
 import { Article } from 'contentlayer/generated';
 
 import { convert } from 'html-to-text';
-import Tag from '../Tag';
+
+import ArticleDate from '../ArticleDate';
+import ArticleCategory from '../ArticleCategory';
+import ArticleTags from '../ArticleTags';
 
 const ArticleCard: FC<{ article: Article }> = ({ article }) => {
   return (
@@ -31,60 +34,15 @@ const ArticleCard: FC<{ article: Article }> = ({ article }) => {
             })}
           </p>
         </div>
-        <div className="basis-1/5 flex flex-col gap-2 text-xs text-gray-500 dark:text-gray-400">
+        <div className="basis-1/5 flex flex-col gap-2">
           <div className="flex flex-row lg:flex-col gap-2">
-            <div className="flex flex-row items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                className="w-4"
-              >
-                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
-              </svg>
-              {new Intl.DateTimeFormat().format(new Date(article.date))}
-            </div>
+            {article.date && <ArticleDate date={new Date(article.date)} />}
             {article.categories && article.categories.length > 0 && (
-              <div className="flex flex-row items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                  className="w-4"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5v-1zM8.5 5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1zM0 11.5A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm4.5.5A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm4.5.5a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"
-                  />
-                </svg>
-                <Link
-                  href={`/categories/${article.categories.join('-')}/pages/1`}
-                >
-                  {article.categories.join('/')}
-                </Link>
-              </div>
+              <ArticleCategory categories={article.categories} />
             )}
-            <div className="flex flex-row flex-wrap gap-1 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                className="w-4"
-              >
-                <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z" />
-                <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z" />
-              </svg>
-              {article.tags?.map((tag) => (
-                <Tag key={tag} tag={tag} />
-              ))}
-            </div>
+            {article.tags && article.tags.length > 0 && (
+              <ArticleTags tags={article.tags} />
+            )}
           </div>
           {/* <div className="rounded bg-gray-200 dark:bg-gray-300 h-28 p-2"></div> */}
         </div>
--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -10,16 +10,21 @@ import { ParsedUrlQuery } from 'querystring';
 
 import { NextSeo } from 'next-seo';
 
+import { useTheme } from 'next-themes';
+
 import { Article } from 'contentlayer/generated';
 import { getArticles } from '@/utils';
 
 import Giscus from '@giscus/react';
-import Tag from '@/components/theme/Tag';
+
 import DefaultLayout from '@/components/theme/DefaultLayout';
+import Heading from '@/components/theme/Heading';
+import ArticleDate from '@/components/theme/ArticleDate';
+import ArticleCategory from '@/components/theme/ArticleCategory';
+import ArticleTags from '@/components/theme/ArticleTags';
+import GoUp from '@/components/theme/GoUp';
 
 import Config from '@/config';
-import { useTheme } from 'next-themes';
-import Heading from '@/components/theme/Heading';
 
 interface Props {
   article: Article;
@@ -98,59 +103,14 @@ const ArticlePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
           <h2 className="text-xl text-gray-800 dark:text-gray-300">
             {article.subtitle}
           </h2>
-          <div className="flex flex-row flex-wrap gap-4 text-xs text-gray-500 dark:text-gray-400">
-            <div className="flex flex-row items-center gap-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                className="w-4"
-              >
-                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
-                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
-              </svg>
-              {new Intl.DateTimeFormat().format(new Date(article.date))}
-            </div>
+          <div className="flex flex-row flex-wrap gap-3 text-xs text-gray-500 dark:text-gray-400">
+            <ArticleDate date={new Date(article.date)} />
             {article.categories && article.categories.length > 0 && (
-              <div className="flex flex-row items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                  className="w-4"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H14a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 2 7h5.5V6A1.5 1.5 0 0 1 6 4.5v-1zM8.5 5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1zM0 11.5A1.5 1.5 0 0 1 1.5 10h1A1.5 1.5 0 0 1 4 11.5v1A1.5 1.5 0 0 1 2.5 14h-1A1.5 1.5 0 0 1 0 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm4.5.5A1.5 1.5 0 0 1 7.5 10h1a1.5 1.5 0 0 1 1.5 1.5v1A1.5 1.5 0 0 1 8.5 14h-1A1.5 1.5 0 0 1 6 12.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm4.5.5a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"
-                  />
-                </svg>
-                <Link
-                  href={`/categories/${article.categories.join('-')}/pages/1`}
-                >
-                  {article.categories.join('/')}
-                </Link>
-              </div>
+              <ArticleCategory categories={article.categories} />
             )}
-            <div className="flex flex-row flex-wrap gap-1 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                className="w-4"
-              >
-                <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z" />
-                <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z" />
-              </svg>
-              {article.tags?.map((tag) => (
-                <Tag key={tag} tag={tag} />
-              ))}
-            </div>
+            {article.tags && article.tags.length > 0 && (
+              <ArticleTags tags={article.tags} />
+            )}
           </div>
         </header>
         <main
@@ -159,22 +119,28 @@ const ArticlePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
         />
       </article>
 
-      <Giscus
-        id="comments"
-        repo="rlj1202/blog"
-        repoId="MDEwOlJlcG9zaXRvcnkyMjg3OTc4MTY="
-        category="Announcements"
-        categoryId="DIC_kwDODaMteM4CS6FK"
-        mapping="pathname"
-        // strict="0"
-        term="Welcome to @giscus/react component!"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="bottom"
-        theme={theme}
-        lang="ko"
-        loading="lazy"
-      />
+      <div className="mb-16">
+        <Giscus
+          id="comments"
+          repo="rlj1202/blog"
+          repoId="MDEwOlJlcG9zaXRvcnkyMjg3OTc4MTY="
+          category="Announcements"
+          categoryId="DIC_kwDODaMteM4CS6FK"
+          mapping="pathname"
+          // strict="0"
+          term="Welcome to @giscus/react component!"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="bottom"
+          theme={theme}
+          lang="ko"
+          loading="lazy"
+        />
+      </div>
+
+      <div className="flex flex-row justify-end">
+        <GoUp />
+      </div>
 
       {/* <h1>Latest</h1> */}
       {/* <div className="post-suggestions">
