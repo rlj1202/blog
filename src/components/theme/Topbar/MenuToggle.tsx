4f4d95670797b9f52405c37096846542e5b29c84
Jisu Sim <rlj1202@gmail.com>
2023-06-23 23:23:26 +0900

    Make clickable area larger

--- src/components/theme/Topbar/MenuToggle.tsx
+++ src/components/theme/Topbar/MenuToggle.tsx
@@ -16,9 +16,9 @@ const MenuToggle: FC = () => {
 
   return (
     <div className="block lg:hidden">
-      <div className="border border-black dark:border-gray-50 rounded w-8 h-8 text-sm bg-gray-200 dark:bg-gray-700 text-black dark:text-gray-50">
-        <div className="w-full h-full flex flex-row justify-center items-center">
-          <button onClick={toggleMenu}>
+      <button onClick={toggleMenu}>
+        <div className="border border-black dark:border-gray-50 rounded w-8 h-8 text-sm bg-gray-200 dark:bg-gray-700 text-black dark:text-gray-50">
+          <div className="w-full h-full flex flex-row justify-center items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -32,9 +32,9 @@ const MenuToggle: FC = () => {
                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
               />
             </svg>
-          </button>
+          </div>
         </div>
-      </div>
+      </button>
 
       <div
         className={`backdrop-blur-md z-10 fixed top-0 left-0 w-full h-full bg-gray-200/70 dark:bg-gray-800/70 ease-in-out duration-200 transform ${
--- src/components/theme/Topbar/ThemeToggle.tsx
+++ src/components/theme/Topbar/ThemeToggle.tsx
@@ -14,9 +14,9 @@ const ThemeToggle: FC = () => {
   };
 
   return (
-    <div className="border border-black dark:border-gray-50 rounded w-8 h-8 bg-gray-200 dark:bg-gray-700 text-black dark:text-gray-50">
-      <div className="w-full h-full flex flex-row justify-center items-center">
-        <button onClick={toggleTheme}>
+    <button onClick={toggleTheme}>
+      <div className="border border-black dark:border-gray-50 rounded w-8 h-8 bg-gray-200 dark:bg-gray-700 text-black dark:text-gray-50">
+        <div className="w-full h-full flex flex-row justify-center items-center">
           {theme === 'light' ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -40,9 +40,9 @@ const ThemeToggle: FC = () => {
               <path d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z" />
             </svg>
           )}
-        </button>
+        </div>
       </div>
-    </div>
+    </button>
   );
 };
 
