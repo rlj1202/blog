1ee3168c9194eef8e50cd527071bb513d39d1c50
Jisu Sim <rlj1202@gmail.com>
2023-06-23 23:20:06 +0900

    Add little animation to GoUp button

--- src/components/theme/GoUp.tsx
+++ src/components/theme/GoUp.tsx
@@ -11,7 +11,9 @@ const GoUp: FC = () => {
   return (
     <div className="border border-black dark:border-gray-50 rounded px-8 py-2 text-sm font-bold text-black dark:text-gray-50">
       <span className="align-middle">
-        <button onClick={scrollToTop}>Go up ↑</button>
+        <button onClick={scrollToTop}>
+          Go up <span className="animate-bounce inline-block">↑</span>
+        </button>
       </span>
     </div>
   );
