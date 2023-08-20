5cff7d53e9bfd17bd99b12e455b133a572cefc78
Jisu Sim <rlj1202@gmail.com>
2023-08-20 17:44:41 +0900

    Add locale to datetime format

--- src/components/theme/ArticleDate.tsx
+++ src/components/theme/ArticleDate.tsx
@@ -5,7 +5,7 @@ const ArticleDate: FC<{ date: Date }> = ({ date }) => {
   return (
     <div className="flex flex-row items-center gap-1 text-xs text-gray-500 dark:text-gray-400">
       <ClockIcon className="w-4" />
-      {new Intl.DateTimeFormat().format(date)}
+      {new Intl.DateTimeFormat('ko-KR').format(date)}
     </div>
   );
 };
