cacea94b0f74d67af65e9e0a1bb923a9ad1f243d
rlj1202 <rlj1202@gmail.com>
2023-08-24 14:47:54 +0900

    Break word in heading

--- src/components/theme/Heading.tsx
+++ src/components/theme/Heading.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 
 const Heading: FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
-    <h1 className="text-6xl font-extrabold text-gray-900 dark:text-gray-50">
+    <h1 className="text-6xl font-extrabold text-gray-900 dark:text-gray-50 break-words">
       <span className="bg-gradient-to-r bg-no-repeat bg-bottom bg-[length:100%_35%] from-red-500/60 to-red-500/60 dark:from-red-500/80 dark:to-red-500/80">
         {children}
       </span>
